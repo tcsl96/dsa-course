a9a5a77313aff5651635b8b3d7a1465fe54830c5
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2023-11-03 12:57:40 -0300

    Implementing prepend, pop and print_info methods. Also, including an example output of the linked list information.

--- src/linked_list.py
+++ src/linked_list.py
@@ -10,50 +10,61 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def append(self, value):
+    def append(self, value) -> None:
         new_node = Node(value)
         self.tail.next = new_node
         self.tail = new_node
         self.length += 1
 
-    def prepend(self):
+    def prepend(self, value) -> None:
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def pop(self) -> None:
+        node_n = self.head
+        while (node_n.next != self.tail):
+            node_n = node_n.next
+        node_n.next = None
+        self.tail = node_n
+        self.length -= 1
+
+    def insert(self, index) -> None:
         pass
 
-    def pop(self):
+    def remove(self, index) -> None:
         pass
 
-    def insert(self, index):
-        pass
-
-    def remove(self, index):
-        pass
+    def print_info(self) -> None:
+        # Printing LL information.
+        # Example output at "ll_out.txt".
+        print("\nLL INFO")
+        print("------------------------------------------------------------")
+        print("LL Head: ", self.head)
+        print("LL Tail: ", self.tail)
+        print("LL Length: ", self.length)
+        node_n = self.head
+        for n in range(1, self.length + 1):
+            print(f"Node {n} Value: {node_n.value}")
+            print(f"Node {n} Pointer: {node_n.next}")
+            node_n = node_n.next
+        print("------------------------------------------------------------")
 
 if __name__ == "__main__":
+    # Creating the linked list.
     ll = LinkedList(3)
-    print("LL Head: ", ll.head)
-    print("LL Tail: ", ll.tail)
-    print("LL Length: ", ll.length)
-    print("Node 1 Value: ", ll.head.value)
-    print("Node 1 Pointer: ", ll.head.next)
+    ll.print_info()
 
+    # Applying the methods.
     ll.append(5)
-    print("\n")
-    print("LL Head: ", ll.head)
-    print("LL Tail: ", ll.tail)
-    print("LL Length: ", ll.length)
-    print("Node 1 Value: ", ll.head.value)
-    print("Node 1 Pointer: ", ll.head.next)
-    print("Node 2 Value: ", ll.tail.value)
-    print("Node 2 Pointer: ", ll.tail.next)
+    ll.print_info()
+
+    ll.prepend(1)
+    ll.print_info()
 
     ll.append(2)
-    print("\n")
-    print("LL Head: ", ll.head)
-    print("LL Tail: ", ll.tail)
-    print("LL Length: ", ll.length)
-    print("Node 1 Value: ", ll.head.value)
-    print("Node 1 Pointer: ", ll.head.next)
-    print("Node 2 Value: ", ll.head.next.value)
-    print("Node 2 Pointer: ", ll.head.next.next)
-    print("Node 3 Value: ", ll.tail.value)
-    print("Node 3 Pointer: ", ll.tail.next)
+    ll.print_info()
+
+    ll.pop()
+    ll.print_info()
