a4a323e9f03c808bd7238548edfb523ba37f25e0
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2023-11-08 15:06:24 -0300

    Implementing insert, remove and revert methods. Also, implementing solutions of leetcode exercises.

--- src/linked_list.py
+++ src/linked_list.py
@@ -4,6 +4,7 @@ class Node:
         self.next = None
 
 class LinkedList:
+    # Main methods.
     def __init__(self, value) -> None:
         new_node = Node(value)
         self.head = new_node
@@ -19,6 +20,7 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+        return True
 
     def prepend(self, value) -> None:
         new_node = Node(value)
@@ -29,44 +31,44 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
         self.length += 1
+        return True
 
     def get(self, index: int) -> None | Node:
         if (index < 0) or (index >= self.length):
-            print("\nThe LL is empty.")
+            print("\nIndex Out of Range.")
             return None
-        node_n = self.head
+        ref = self.head
         for _ in range(index):
-            node_n = node_n.next
-        return node_n
+            ref = ref.next
+        return ref
 
     def set(self, index: int, value) -> None:
-        node_n = self.get(index)
-        if node_n is None:
-            return None
-        node_n.value = value
+        ref = self.get(index)
+        if (ref is not None):
+            ref.value = value
 
-    def pop(self) -> None | Node:
+    def pop(self) -> bool | Node:
         if (self.length == 0):
             print("\nThe LL is empty.")
-            return None
+            return False
         elif (self.length == 1):
             popped_node = self.tail
             self.clear()
             return popped_node
         else:
-            node_n = self.head
-            while (node_n.next != self.tail):
-                node_n = node_n.next
-            popped_node = node_n.next
-            node_n.next = None
-            self.tail = node_n
+            ref = self.head
+            while (ref.next != self.tail):
+                ref = ref.next
+            popped_node = ref.next
+            ref.next = None
+            self.tail = ref
             self.length -= 1
             return popped_node
 
-    def pop_first(self) -> None | Node:
+    def pop_first(self) -> bool | Node:
         if (self.length == 0):
             print("\nThe LL is empty.")
-            return None
+            return False
         elif (self.length == 1):
             popped_node = self.head
             self.clear()
@@ -78,16 +80,51 @@ class LinkedList:
             self.length -= 1
             return popped_node
 
-    def insert(self, index: int) -> None:
-        pass
+    def insert(self, index: int, value) -> bool:
+        if (index < 0) or (index > self.length):
+            return False
+        if (index == 0):
+            return self.prepend(value)
+        elif (index == self.length):
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            ref = self.get(index - 1)
+            new_node.next = ref.next
+            ref.next = new_node
+            self.length += 1
+            return True
 
-    def remove(self, index: int) -> None:
-        pass
+    def remove(self, index: int) -> bool | Node:
+        if (index < 0) or (index >= self.length):
+            return False
+        if (index == 0):
+            return self.pop_first()
+        elif (index == (self.length - 1)):
+            return self.pop()
+        else:
+            ref = self.get(index - 1)
+            removed_node = ref.next
+            ref.next = removed_node.next
+            removed_node.next = None
+            self.length -= 1
+            return removed_node
 
     def clear(self) -> None:
         self.head = None
         self.tail = None
         self.length = 0
+
+    def revert(self) -> None:
+        ref = self.head
+        self.head = self.tail
+        self.tail = ref
+        before = None
+        while (ref is not None):
+            after = ref.next
+            ref.next = before
+            before = ref
+            ref = after
 
     def print_info(self) -> None:
         # Printing LL information.
@@ -106,6 +143,89 @@ class LinkedList:
             print(f"Node {n} Pointer: {node_n.next}")
             node_n = node_n.next
         print("-"*60)
+
+    # Leetcode Exercises.
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while (fast.next is not None) and (fast is not None):
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def has_loop(self):
+        """
+        Floyd's cycle-finding algorithm, often referred to as the "tortoise and the hare" technique.
+        """
+        slow = self.head
+        fast = self.head
+        while (fast.next is not None) and (fast is not None):
+            slow = slow.next
+            fast = fast.next.next
+            if (slow == fast):
+                return True
+        return False
+
+    def find_kth_from_end(self, index):
+        ref = self.head   
+        after = self.head 
+        for _ in range(index):
+            if (after is None):
+                return None
+            after = after.next
+        while (after is not None):
+            ref = ref.next
+            after = after.next
+        return ref
+
+    def partition_list(self, value):
+        if (self.length == 0):
+            return None
+        dummy1 = Node(0)
+        dummy2 = Node(0)
+        next1 = dummy1
+        next2 = dummy2
+        ref = self.head
+        while (ref is not None):
+            if (ref.value < value):
+                next1.next = ref
+                next1 = ref
+            else:
+                next2.next = ref
+                next2 = ref
+            ref = ref.next
+        next1.next = dummy2.next
+        next2.next = None
+        self.head = dummy1.next
+
+    def remove_duplicates(self):
+        nodes = {}
+        ref = self.head
+        before = None
+        while (ref is not None):
+            if (nodes.get(ref.value) is not None):
+                before.next = ref.next
+                ref.next = None
+                ref = before.next
+                self.length -= 1
+            else:
+                nodes[ref.value] = 0
+                before = ref
+                ref = ref.next
+
+    def binary_to_decimal(self):
+        if (self.length == 0):
+            return None
+        dec = 0
+        ref = self.head
+        for index in range(1, self.length + 1):
+            dec += ref.value*2**(self.length - index)
+            ref = ref.next
+        return dec
+
+    # TODO: Implement this method.
+    def reverse_between(self, start_index, end_index):
+        pass
 
 if __name__ == "__main__":
     # Creating the linked list.
